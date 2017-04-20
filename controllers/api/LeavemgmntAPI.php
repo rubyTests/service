44fe3ab333b6a0aa5301d0b924c4761f88ea353a
vijayaraj94 <vijayaraj@appnlogic.com>
2017-04-20 16:09:12 +0530

    LEAEV MANAGEMENT

--- controllers/api/LeavemgmntAPI.php
+++ controllers/api/LeavemgmntAPI.php
@@ -15,21 +15,21 @@ class LeavemgmntAPI extends REST_Controller {
 		checkAccess();
     }
 
-    // Leave category
+    // Leave Type
 	
-	function leaveCategory_post(){
+	function leaveType_post(){
 		$id = $this->post('id');
 		$data['cat_name']=$this->post('cat_name');
 		$data['cat_code']=$this->post('cat_code');
 		if($id==NULL){
-			$result=$this->leave_mgmnt_model->addleaveCategoryDetails($data);
+			$result=$this->leave_mgmnt_model->addleaveTypeDetails($data);
 			if($result['status']==true){
 				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
 			}else{
 				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
 			}
 		}else{
-			$result=$this->leave_mgmnt_model->editLeaveCategoryDetails($id,$data);
+			$result=$this->leave_mgmnt_model->editLeaveTypeDetails($id,$data);
 			if($result['status']==true){
 				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
 			}else{
@@ -38,10 +38,10 @@ class LeavemgmntAPI extends REST_Controller {
 		}
 	}
 	
-	function leaveCategory_get(){
+	function leaveType_get(){
 		$id = $this->get('id');
 		if($id==NULL){
-			$users=$this->leave_mgmnt_model->fetchEmployeeCategoryDetails();
+			$users=$this->leave_mgmnt_model->fetchLeaveTypeDetails();
 			if (!empty($users)){
 				$this->set_response(['status' =>TRUE,'data'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
 			}
@@ -61,7 +61,7 @@ class LeavemgmntAPI extends REST_Controller {
 		}
     }
 	
-	function leaveCategory_delete(){
+	function leaveType_delete(){
 		$id = $this->delete('id');
 		if($id==NULL){
 				$this->set_response([
@@ -69,7 +69,263 @@ class LeavemgmntAPI extends REST_Controller {
 				'message' => 'Record could not be found'
 				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
 		}else{
-			$users=$this->leave_mgmnt_model->deleteLeaveCategoryDetails($id);
+			$users=$this->leave_mgmnt_model->deleteLeaveTypeDetails($id);
+			if ($users!=0){
+				$this->set_response(['status' =>TRUE,'message'=>'Record deleted successfully'], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Category Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+
+    // leave Entitlement
+    function leaveEntitlement_post(){
+    	// print_r($this->post());exit;
+		$id = $this->post('id');
+		$data['dept_id']=$this->post('dept_id');
+		$data['emp_id']=$this->post('emp_id');
+		$data['leave_list']=$this->post('leave_list');
+		if($id==NULL){
+			$result=$this->leave_mgmnt_model->addleaveEntitlementDetails($data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}else{
+			$result=$this->leave_mgmnt_model->editLeaveEntitlementDetails($id,$data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}
+	}
+	function leaveEntitlement_get(){
+		$id = $this->get('id');
+		if($id==NULL){
+			$users=$this->leave_mgmnt_model->fetchEntitlementViewDetails();
+			if (!empty($users)){
+				$this->set_response(['status' =>TRUE,'data'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+
+		}else{
+			$users=$this->leave_mgmnt_model->getParticularDetails($id);
+			if (!empty($users)){
+				$this->set_response(['status' =>TRUE,'message'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+
+    function leaveEntitlement_delete(){
+		$id = $this->delete('id');
+		if($id==NULL){
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->deleteLeaveEntitlement($id);
+			if ($users!=0){
+				$this->set_response(['status' =>TRUE,'message'=>'Record deleted successfully'], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Category Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+
+    function leaveEntitlementDelete_delete(){
+    	$id = $this->delete('id');
+		if($id==NULL){
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->deleteParticularCategory($id);
+			if ($users!=0){
+				$this->set_response(['status' =>TRUE,'message'=>'Record deleted successfully'], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Category Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+    function fetchEmployeeLeaveType_get(){
+    	$id = $this->get('id');
+		if($id==NULL){
+			$this->set_response([
+			'status' => FALSE,
+			'message' => 'Record could not be found'
+			], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->getemplyeeleavetypelist($id);
+			if (!empty($users)){
+				$this->set_response(['status' =>TRUE,'message'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+    function leaveTypelistandcount_get(){
+    	$id = $this->get('id');
+		if($id==NULL){
+			$this->set_response([
+			'status' => FALSE,
+			'message' => 'Record could not be found'
+			], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->fetchLeaveTypeList_Count($id);
+			if (!empty($users)){
+				$this->set_response(['status' =>TRUE,'message'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+
+     function applyLeave_post(){
+    		// print_r($this->post());exit;
+		$id = $this->post('id');
+		$data['leave_typeID']=$this->post('leave_typeID');
+		$data['from_date']=$this->post('from_date');
+		$data['upto_date']=$this->post('upto_date');
+		$data['description']=$this->post('description');
+		$data['total_leave']=$this->post('total_leave');
+		$data['employee_id']=$this->post('employee_id');
+		if($id==NULL){
+			$result=$this->leave_mgmnt_model->applyLeaveDetails($data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}else{
+			// $result=$this->leave_mgmnt_model->editLeaveEntitlementDetails($id,$data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}
+	}
+	function applyLeave_get(){
+    	$id = $this->get('id');
+		if($id==NULL){
+			$this->set_response([
+			'status' => FALSE,
+			'message' => 'Record could not be found'
+			], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->getViewdataforApplyLeave($id);
+			if (!empty($users)){
+				$this->set_response(['status' =>TRUE,'message'=>$users], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+			else
+			{
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record Detail could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+			}
+		}
+    }
+
+    function leaveApplicationDetails_get(){
+    	$result=$this->leave_mgmnt_model->getViewdataforLeaveApplication();
+		if (!empty($result)){
+			$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+		}
+		else
+		{
+			$this->set_response([
+			'status' => FALSE,
+			'message' => 'Record Detail could not be found'
+			], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}
+    }
+    function fetchLeaveDetailList_get(){
+    	$empId = $this->get('id');
+    	$lineid = $this->get('lineid');
+    	if(!empty($empId) && !empty($lineid)){
+    		$result=$this->leave_mgmnt_model->getleavetypelistforEmployee($empId,$lineid);
+			if (!empty($result)){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+			}
+    	}else {
+    		$this->set_response([
+			'status' => FALSE,
+			'message' => 'Record Detail could not be found'
+			], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+    	}
+    }
+    function updateLeaveStatus_post(){
+		$id=$this->post('id');
+		$data['check_status']=$this->post('check_status');
+		if($id){
+			$result=$this->leave_mgmnt_model->updateStutus($id,$data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}else{
+			// $result=$this->leave_mgmnt_model->editLeaveEntitlementDetails($id,$data);
+			if($result['status']==true){
+				$this->set_response(['status' =>TRUE,'data'=>$result], REST_Controller::HTTP_CREATED);
+			}else{
+				$this->set_response(['status' =>FALSE,'data'=>"Failure"], REST_Controller::HTTP_CREATED);
+			}
+		}
+    }
+    function leaveApplicationDetails_delete(){
+		$id = $this->delete('id');
+		$bal_id = $this->delete('bal_id');
+		if($id==NULL){
+				$this->set_response([
+				'status' => FALSE,
+				'message' => 'Record could not be found'
+				], REST_Controller::HTTP_NOT_FOUND); // NOT_FOUND (404) being the HTTP response code
+		}else{
+			$users=$this->leave_mgmnt_model->deleteLeaveApplicationDetails($id,$bal_id);
 			if ($users!=0){
 				$this->set_response(['status' =>TRUE,'message'=>'Record deleted successfully'], REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
 			}
--- models/leave_mgmnt_model.php
+++ models/leave_mgmnt_model.php
@@ -2,8 +2,8 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 	class leave_mgmnt_model extends CI_Model {
 
-		// Category Details 		
-		public function addleaveCategoryDetails($value){
+		// Leave type Details 		
+		public function addleaveTypeDetails($value){
 			$name=$value['cat_name'];
 			$sql="SELECT * FROM leave_type where NAME='$name'";
 			$result = $this->db->query($sql, $return_object = TRUE)->result_array();
@@ -22,7 +22,7 @@
 			}			
 	    }
 		
-		public function editLeaveCategoryDetails($id,$value){
+		public function editLeaveTypeDetails($id,$value){
 			$sql="SELECT * FROM leave_type where ID='$id'";
 			$result = $this->db->query($sql, $return_object = TRUE)->result_array();			
 			if($result[0]['NAME']==$value['cat_name']){
@@ -51,15 +51,155 @@
 			}
 		}
 		
-		public function fetchEmployeeCategoryDetails(){
+		public function fetchLeaveTypeDetails(){
 			$sql="SELECT * FROM leave_type";
 			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
 		}
 		
-		public function deleteLeaveCategoryDetails($id){
+		public function deleteLeaveTypeDetails($id){
 			$sql="DELETE FROM leave_type where ID='$id'";
 			$result = $this->db->query($sql);
 	    	return $this->db->affected_rows();
 		}
+
+		// leave Entilement
+
+		public function addleaveEntitlementDetails($value){
+			// print_r($value['leave_list']);exit;
+			// $list=$value['leave_list'];
+			for($i=0;$i<count($value['leave_list']);$i++){
+				$data = array(
+				   'LEAVE_TYPE_ID' => $value['leave_list'][$i]['leave_category'],
+				   'LEAVETYPE_COUNT' => $value['leave_list'][$i]['leave_count'],
+				   'VALID_FROM' => $value['leave_list'][$i]['valid_from'],
+				   'EMP_PROFILE_ID' => $value['emp_id']
+				);
+				$this->db->insert('leave_entitlement', $data); 
+			}
+			return array('status'=>true, 'message'=>"Record Inserted Successfully");
+	    }
+
+	    public function editLeaveEntitlementDetails($id,$value){
+			if($id){
+				for($i=0;$i<count($value['leave_list']);$i++){
+					if (isset($value['leave_list'][$i]['EMP_PROFILE_ID'])){  
+						$data = array(
+						   'LEAVE_TYPE_ID' => $value['leave_list'][$i]['LEAVE_TYPE_ID'],
+						   'LEAVETYPE_COUNT' => $value['leave_list'][$i]['LEAVETYPE_COUNT'],
+						   'VALID_FROM' => $value['leave_list'][$i]['VALID_FROM'],
+						   'EMP_PROFILE_ID' => $value['emp_id']
+						);
+						$this->db->where('ID', $value['leave_list'][$i]['ID']);
+						$this->db->update('leave_entitlement', $data);
+					}else {
+						$data = array(
+						   'LEAVE_TYPE_ID' => $value['leave_list'][$i]['LEAVE_TYPE_ID'],
+						   'LEAVETYPE_COUNT' => $value['leave_list'][$i]['LEAVETYPE_COUNT'],
+						   'VALID_FROM' => $value['leave_list'][$i]['VALID_FROM'],
+						   'EMP_PROFILE_ID' => $value['emp_id']
+						);
+						$this->db->insert('leave_entitlement', $data);
+					}
+				}
+				// exit;
+				return array('status'=>true, 'message'=>"Record Updated Successfully");
+			}
+			
+	    }
+
+	    function fetchEntitlementViewDetails(){
+	    	$sql="SELECT * FROM leave_entitlement_view";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+	    }
+	    function getParticularDetails($id){
+	    	$sql="SELECT * FROM leave_entitlement_view where EMP_PROFILE_ID='$id'";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+	    }
+	    public function deleteLeaveEntitlement($id){
+	    	$sql="SELECT * FROM leave_entitlement where EMP_PROFILE_ID='$id'";
+			$result = $this->db->query($sql, $return_object = TRUE)->result_array();
+			foreach ($result as $key => $value) {
+				$ent_id=$value['ID'];
+				$delete="DELETE FROM leave_entitlement where ID='$ent_id'";
+				$result1 = $this->db->query($delete);
+			}
+			return $this->db->affected_rows();
+		}
+		function deleteParticularCategory($id){
+			$sql="DELETE FROM leave_entitlement where ID='$id'";
+			$result = $this->db->query($sql);
+			return $this->db->affected_rows();	    	
+		}
+		function getemplyeeleavetypelist($id){
+			$sql="SELECT leave_entitlement.ID,leave_entitlement.LEAVE_TYPE_ID,leave_entitlement.EMP_PROFILE_ID,leave_entitlement.LEAVETYPE_COUNT,leave_entitlement.VALID_FROM,
+				(select NAME from leave_type where ID=leave_entitlement.LEAVE_TYPE_ID) AS LEAVE_NAME,
+				(select CODE from leave_type where ID=leave_entitlement.LEAVE_TYPE_ID) AS LEAVE_CODE
+				FROM leave_entitlement where EMP_PROFILE_ID='$id'";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+		}
+
+		function fetchLeaveTypeList_Count($id){
+			$sql="select leave_entitlement.EMP_PROFILE_ID,leave_entitlement.LEAVETYPE_COUNT,
+					leave_type.ID AS LEAVE_TYPE_ID, leave_type.NAME AS LEAVE_CATEGORY
+					from leave_entitlement
+					join leave_type on leave_entitlement.LEAVE_TYPE_ID=leave_type.ID
+					where EMP_PROFILE_ID='$id'";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+		}
+
+		public function applyLeaveDetails($value){
+			$typeID=$value['leave_typeID'];
+			$empID=$value['employee_id'];
+			$val = $value['total_leave'];
+			$res=explode("-",$val);
+			$totaleLeave=$res[1];
+			$data = array(
+			   'LEAVE_TYPE_ID' => $value['leave_typeID'],
+			   'EMP_PROFILE_ID' => $value['employee_id'],
+			   'DESCRIPTION' => $value['description'],
+			   'FROM_DATE' => $value['from_date'],
+			   'TOTAL_LEAVE' => $totaleLeave,
+			   'TO_DATE' => $value['upto_date'],
+			   'STATUS' => 'Pending',
+			);
+			$this->db->insert('employee_leave', $data);
+			return array('status'=>true, 'message'=>"Record Inserted Successfully"); 		
+	    }
+
+	    function getViewdataforApplyLeave($id){
+	    	$sql="select SUM(employee_leave.TOTAL_LEAVE) AS TOTAL_LEAVE,
+				(select LEAVETYPE_COUNT from leave_entitlement where EMP_PROFILE_ID=employee_leave.EMP_PROFILE_ID and LEAVE_TYPE_ID=employee_leave.LEAVE_TYPE_ID) AS TOTAL_ALLOC_LEAVE,
+				(select NAME from leave_type where ID=employee_leave.LEAVE_TYPE_ID) AS LEAVE_NAME
+				from employee_leave where EMP_PROFILE_ID='$id' GROUP BY LEAVE_TYPE_ID HAVING count(LEAVE_TYPE_ID)";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+	    }
+	    function getViewdataforLeaveApplication(){
+	    	$sql="select employee_leave.ID,employee_leave.EMP_PROFILE_ID,employee_leave.FROM_DATE,employee_leave.TO_DATE,employee_leave.STATUS,employee_leave.TOTAL_LEAVE,(select concat(FIRSTNAME,' ',LASTNAME) from profile where ID=employee_leave.EMP_PROFILE_ID) AS EMPLOYEE_NAME,(select ID from leave_type where ID=employee_leave.LEAVE_TYPE_ID) AS LEAVE_ID,(select NAME from leave_type where ID=employee_leave.LEAVE_TYPE_ID) AS LEAVE_NAME,(select ID from employee_leave_balance where LEAVE_TYPE_ID=employee_leave.LEAVE_TYPE_ID) AS EMP_L_BAL_ID from employee_leave";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+	    }
+	     function getleavetypelistforEmployee($empID,$lineid){
+	    	$sql="select employee_leave.ID,employee_leave.EMP_PROFILE_ID,employee_leave.LEAVE_TYPE_ID,
+				employee_leave.DESCRIPTION,employee_leave.FROM_DATE,employee_leave.TO_DATE,employee_leave.STATUS,
+				(select NAME from leave_type where ID=employee_leave.LEAVE_TYPE_ID) AS LEAVE_NAME
+				from employee_leave where employee_leave.ID='$lineid' and employee_leave.EMP_PROFILE_ID='$empID'";
+			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
+	    }
+	    function updateStutus($id,$value){
+	    	$data = array(
+			   'STATUS' => $value['check_status']
+			);
+			$this->db->where('ID', $id);
+			$this->db->update('employee_leave', $data); 
+			return array('status'=>true, 'message'=>"Record Updated Successfully");
+	    }
+	    function deleteLeaveApplicationDetails($id,$bal_id){
+	    	$sql="DELETE FROM employee_leave where ID='$id'";
+			$result = $this->db->query($sql);
+			if($result==1){
+				$sql="DELETE FROM employee_leave_balance where ID='$bal_id'";
+				$result = $this->db->query($sql);
+				return $this->db->affected_rows();
+			}
+	    }
 	}
 ?>
