6eb77138aafad8f57dd86c37499de217a81d83b8
Karthik Bala <karthik@appnlogic.com>
2017-07-24 18:23:52 +0530

    bulk sms send

--- models/setting_model.php
+++ models/setting_model.php
@@ -18,5 +18,49 @@
 			$sql="SELECT DISTINCT PROFILE_ID,(SELECT CONCAT(FIRSTNAME,' ',LASTNAME) FROM PROFILE WHERE ID=PROFILE_ID) AS FULLNAME FROM employee_profile";
 			return $result = $this->db->query($sql, $return_object = TRUE)->result_array();
 		}
+		
+		// Bulk SMS sending
+		public function bulkSmsSent($values){
+			$batchId=$values['batchId'];
+			$sql="SELECT PHONE_NO_1 FROM profile WHERE ID IN (SELECT PROFILE_ID FROM student_profile WHERE COURSEBATCH_ID IN($batchId)) AND PHONE_NO_1 IS NOT NULL";
+			return $res = $this->db->query($sql, $return_object = TRUE)->result_array();
+		}
+		
+		public function bulkSmsSentDetials($values,$cDetails){
+			$data = array(
+				'COURSE_ID' => $cDetails['courseId'],
+				'BATCH_ID' => $cDetails['batchId'],
+				'MESSAGE' => $cDetails['msg'],
+				'TOTAL' => $values['data']['total_count'],
+				'SUCCESS' => $values['data']['queued_count']
+			);
+			$this->db->insert('bulksms', $data);
+			$bulksmsId= $this->db->insert_id();
+			if($bulksmsId){
+				foreach($values['data']['messages'] as $value){
+					if(isset($value['date'])){
+						$date=date('Y-m-d',$value['date']);
+					}else{
+						$date=NULL;
+					}
+					
+					if(isset($value['carrier'])){
+						$network=$value['carrier'];
+					}else{
+						$network=NULL;
+					}
+					
+					$data = array(
+						'BULKSMS_ID' => $bulksmsId,
+						'PHONE' => $value['to'],
+						'DATE' => $date,
+						'NETWORK' => $network,
+						'STATUS' => $value['status']
+					);
+					$this->db->insert('bulksms_details', $data);
+				}
+				return true;
+			}
+		}
 	}
 ?>
