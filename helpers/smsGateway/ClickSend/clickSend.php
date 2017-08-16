fd0c4ab39a234dcbcd169b0510dd411a5671fe78
Karthik Bala <karthik@appnlogic.com>
2017-08-16 13:32:37 +0530

    added bulk sms option

--- helpers/smsGateway/ClickSend/clickSend.php
+++ helpers/smsGateway/ClickSend/clickSend.php
@@ -33,4 +33,49 @@ function clickSend($phone,$msg){
 	}
 }
 
+function bulkSend($phone,$msg){
+	
+	try {
+		// Prepare ClickSend client.
+		// $client = new \ClickSendLib\ClickSendClient('Ruby', '5C043826-09BB-0D0B-AE12-CFC0B5B7AEB8');
+		$client = new \ClickSendLib\ClickSendClient('abdul', 'BAD59387-00AE-F814-1FC3-E58281653394');
+
+		// Get SMS instance.
+		$sms = $client->getSMS();
+
+		// The payload.
+		$bulk=[];
+		foreach($phone as $value){
+			$messages = array(
+				"source" => "php",
+				"from" => "sendmobile",
+				"body" => $msg,
+				"to" => "+91".$value['PHONE_NO_1'],
+			);
+			array_push($bulk,$messages);
+		}
+		
+		// $messages =  [
+			// [
+				// "source" => "php",
+				// "from" => "sendmobile",
+				// "body" => "Test Send",
+				// "to" => "+91 9952322349",
+			// ]
+		// ];
+		
+		// print_r($bulk);exit;
+		// Send SMS.
+		return $response = $sms->sendSms(['messages' => $bulk]);
+		// return $response = $sms->sendSms(['messages' => $messages]);
+
+		//print_r($response);
+
+	} catch(\ClickSendLib\APIException $e) {
+
+	   // print_r($e->getResponseBody());
+
+	}
+}
+
 #END OF PHP FILE
